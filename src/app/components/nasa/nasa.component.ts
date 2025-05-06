8127035bb04db9b4d9d0f061602946d2a3a03124
Richard-Cambe <richard.cambe@outlook.fr>
2025-05-06 21:52:29 +0200

    nasa fonctionne, mais video via lien youtube en retour de l'api donc image pas visible...

--- src/app/components/nasa/nasa.component.ts
+++ src/app/components/nasa/nasa.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, WritableSignal, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '@/services/api.service';
+
 
 @Component({
   selector: 'app-nasa',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './nasa.component.html',
   styleUrl: './nasa.component.css'
 })
-export class NasaComponent {
 
+export class NasaComponent implements OnInit {
+  private apiService = inject(ApiService);
+  nasaData: WritableSignal<any> = signal(undefined);
+
+  ngOnInit(): void {
+    this.apiService.getNasaPic().subscribe({
+      next: (data: any) => this.nasaData.set(data),
+      error: () => console.error("J'ai pas la photo")
+    })
+  }
 }
